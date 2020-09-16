f2aef0f3fa7da9c29a0e69879aee9cc8aa76758e
c-apple <c-apple@localhost>
2020-09-17 00:47:01 +0530

    Wilkef-3 : QuestionLevelController class logging and handling exception

--- src/main/java/com/wilkef/ecrack/setup/controller/QuestionLevelController.java
+++ src/main/java/com/wilkef/ecrack/setup/controller/QuestionLevelController.java
@@ -1,6 +1,7 @@
 package com.wilkef.ecrack.setup.controller;
 
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,20 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.wilkef.ecrack.setup.dao.QuestionLevelDao;
 import com.wilkef.ecrack.setup.dto.QuestionLevelDataDTO;
 
-
 @RestController
 @RequestMapping("/questionLevel")
 public class QuestionLevelController {
 
 	private static final Logger LOG = Logger.getLogger(QuestionLevelController.class.getName());
+	List<QuestionLevelDataDTO> allQuestionLevel = null;
 
 	@Autowired
 	private QuestionLevelDao questionDao;
 
 	@GetMapping(value = "/getDifficultyCode")
-	public List<QuestionLevelDataDTO> findDifficultyCode(){
-		LOG.info("No input");
-		List<QuestionLevelDataDTO> allQuestionLevel = questionDao.findQuestionLevel();
+	public List<QuestionLevelDataDTO> findDifficultyCode() {
+		LOG.info("Inside find difficulty code");
+		try {
+			LOG.log(Level.INFO, () -> "Before getting question information .. ");
+			if (allQuestionLevel == null)
+				allQuestionLevel = questionDao.findQuestionLevel();
+		} catch (Exception exception) {
+			LOG.log(Level.SEVERE, () -> "Something wrong while fetching the information : " + exception.getMessage());
+		}
 		return allQuestionLevel;
 	}
 }
